052d13b99379eb3989e451065027a3cb20eae14f
hernan verar <hernanverar@gmail.com>
2023-03-31 12:15:32 -0700

    WIP

--- js/script.js
+++ js/script.js
@@ -1,23 +1,20 @@
 
 
   function beep (text) {
-    let beepArray = text.split("");
-    let outputbox = text;
-    beepArray.forEach(function(element) {
-      if (i = 0 || i >100 ++) () {
-        outputbox = replace (0);
-      }
+    const beepArray = text.split("");
+    const doubleBeepArray = []
+    if (beepArray.length < 0 && beepArray > 1000) { 
+      for(let index = -0; index <beepArray.length; index++);
 
-    });
-    return outputbox;
+    }
+    return 0;
   
   }
 
-// window.addEventListener("load", function (e) {
-//   const input1 =document.getElementById("input").value;
-//   const results = beep ("input");
-//   document.getElementById("outputbox").innertext = results;
-//   e.preventDefault();
-// });
+window.addEventListener("load", function (e) {
+  const input1 =document.getElementById("input").value;
+  const results = beep ("beep");
+  document.getElementById("outputbox").innertext = results;
+  e.preventDefault();
+});
   
-// }
